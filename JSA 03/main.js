93d1d1a8a523c452477148cd98c8519933d3c334
Gia Bao <giabaosocool@gmail.com>
2021-03-28 10:50:21 +0700

    Day 4

---
+++ JSA 03/main.js
@@ -0,0 +1,10 @@
+var vidu = document.getElementById("vidu4");
+console.log("vidu");
+
+vidu.onclick = function() {
+    if(vidu.classList.contains("nenXam")) {
+        vidu.classList.remove("nenXam")
+    } else {
+        vidu.classList.add("nenXam")
+    }
+}
